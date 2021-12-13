cc6799fb7bc8e81165f17b27ab2cc1c6898ae979
ffortress <ffortress2@gmail.com>
2021-12-13 16:19:28 +0900

    .
    .

--- bot.py
+++ bot.py
@@ -1,21 +1,59 @@
+import asyncio
 import discord
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 from discord.ext import commands
-import os
+from urllib import parse
 
-client = commands.Bot(command_prefix = '-')
+client = discord.Client()
+pageurl = "https://lostark.game.onstove.com/Profile/Character/"
 
 @client.event
 async def on_ready():
+    print("Logged in as ") #봇의 아이디, 닉네임이 출력
+    print(client.user.name)
+    print(client.user.id)
 
-  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
-  await client.change_presence(status=discord.Status.online)
+@client.event
+async def on_message(message):
 
-  await client.change_presence(activity=discord.Game(name="게임 하는중"))
-  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
-  
-  print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
+    if message.author.bot:
+        return None    
+    
+    if message.content.startswith('!검색'):
+        await id_search(message)
 
+@client.event
+async def id_search(message):
+    id = message.author.id
+    channel = message.channel   
+    url = pageurl+parse.quote(message.content[4:]) 
+    html = urlopen(url)
+    bsObject = BeautifulSoup(html, "html.parser")
+
+
+    inumber = 0
+    ijobnumber = 0
+    img_alt = []
+    for chname in bsObject.find_all("div", {"class":"content content--profile"})[0].find_all("ul", {"class":"profile-character-list__char"}):        
+        for name in chname.find_all("span"):            
+            
+            for img_tag in name.find_all('img'):
+                img_alt.append(str(img_tag.get('alt')))               
+
+            if inumber % 2 != 0:               
+                thisurl = pageurl + parse.quote(re.sub( "\<span>|\</span>","", str(name)))                              
+                thishtml = urlopen(thisurl)
+                thisbsObject = BeautifulSoup(thishtml, "html.parser")
+                
+                lvstr = thisbsObject.find_all("div", {"class":"level-info2__item"})[0].find_all("span")[1]
+                
+                await channel.send( re.sub( "\<span>|\</span>|\<small>|\</small>","", str(name) + "/" + img_alt[ijobnumber] + "/" +str(lvstr)))
+                ijobnumber+=1            
+
+            inumber+=1
+
+    return
 
 client.run(os.environ['token'])
